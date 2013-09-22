4a2bfc117c9b20364c2f553f28243a96336ba93f
Dan Cunningham <dan@pondera.me>
2013-09-22 13:35:30 +0100

    Colours

--- models/restaurant.js
+++ models/restaurant.js
@@ -7,7 +7,7 @@ var FeedBackSchema = new Schema({
 		created : { type: Date, default: Date.now() }
 	});
 
-var RestauarantSchema = new Schema({
+var RestaurantSchema = new Schema({
 	id: ObjectId,
 		name: { type: String, required: true,  trim: true },
 		location: { type: String, required: true,  trim: true },
@@ -17,10 +17,107 @@ var RestauarantSchema = new Schema({
 		
 	});
 
-	RestauarantSchema.virtual('likes').get(function() {
-	    // check for a future lockUntil timestamp
-	    return this.feedBacks.find({like: true}).count();
+	RestaurantSchema.virtual('likes').get(function() {
+		// DC: I know this is silly
+		// Wanted to do something neat like:
+	    //return this.feedBacks.find({like: 1}).count();
+		positive = 0;
+		negative = 0;
+		total = 0;
+		this.feedBacks.forEach(function(feedback){
+			if (feedback.like == 1)
+				positive++;
+			else
+				negative++;
+			total++;
+		});
+		return positive;
 	});
 
-module.exports = mongoose.model('Restaurant', RestauarantSchema);
+	RestaurantSchema.virtual('unlikes').get(function() {
+		// DC: I know this is silly
+		positive = 0;
+		negative = 0;
+		total = 0;
+		this.feedBacks.forEach(function(feedback){
+			if (feedback.like == 1)
+				positive++;
+			else
+				negative++;
+			total++;
+		});
+		return negative;
+	});
+
+	RestaurantSchema.virtual('total').get(function() {
+		// DC: I know this is silly
+		positive = 0;
+		negative = 0;
+		total = 0;
+		this.feedBacks.forEach(function(feedback){
+			if (feedback.like == 1)
+				positive++;
+			else
+				negative++;
+			total++;
+		});
+		return total;
+	});
+
+	RestaurantSchema.virtual('avg').get(function() {
+		// DC: I know this is silly
+		positive = 0;
+		negative = 0;
+		total = 0;
+		this.feedBacks.forEach(function(feedback){
+			if (feedback.like == 1)
+				positive++;
+			else
+				negative++;
+			total++;
+		});
+		if (total == 0) return 0;
+		return (positive-negative)/total;
+	});
+
+	RestaurantSchema.virtual('rgb').get(function() {
+		var r, g, b = 0;
+		a = this.avg;
+		b = 0;
+		if (a == 0) {
+			r = 255;
+			g = 255;
+		} else if (a > 0) {
+			r = (1-a) * 255;
+			g = 255;
+		} else if (a < 0) {
+			r = 255;
+			g = (1+a) * 255;
+		}
+		rgb = "(" + Math.floor(r) + "," + Math.floor(g) + "," + Math.floor(b) + ")";
+		return rgb;
+	});
+
+	RestaurantSchema.virtual('hex').get(function() {
+		var r, g, b = 0;
+		a = this.avg;
+		b = 0;
+		if (a == 0) {
+			r = 255;
+			g = 255;
+		} else if (a > 0) {
+			r = (1-a) * 255;
+			g = 255;
+		} else if (a < 0) {
+			r = 255;
+			g = (1+a) * 255;
+		}
+		hex_r = (r < 16 ? "0" : "") + Math.floor(r).toString(16);
+		hex_g = (g < 16 ? "0" : "") + Math.floor(g).toString(16);
+		hex_b = (b < 16 ? "0" : "") + Math.floor(b).toString(16);
+		hex = "#" + hex_r + hex_g + hex_b;
+		return hex;
+	});
+
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
 module.exports = mongoose.model('FeedBack', FeedBackSchema);
